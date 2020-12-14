207a8aeb1ee611f7992489c2d522a998aeae0bda
ricoandreas <ricodummy3@gmail.com>
2020-12-14 20:57:58 +0700

    user menu rev

--- component/CashlessPayment.js
+++ component/CashlessPayment.js
@@ -1,53 +1,70 @@
 import React from 'react';
-import {View, Text, StyleSheet,TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 function CashlessPayment() {
   const navigation = useNavigation();
   return (
-    <View style= {{flex:1, backgroundColor: '#FFF1E1'}}>
+    <View style={{flex: 1, backgroundColor: '#FFF1E1'}}>
       <View style={{backgroundColor: '#FFB84C'}}>
-          <Text style= {{
-                fontSize: 24, 
-                color: 'white', 
-                fontWeight: 'bold',
-                marginTop: 11,
-                marginBottom: 12,
-                alignItems: 'center',
-                textAlign: 'center',
-            }}>
-            CASHLESS PAYMENT
-          </Text>  
+        <Text
+          style={{
+            fontSize: 24,
+            color: 'white',
+            fontWeight: 'bold',
+            marginTop: 11,
+            marginBottom: 12,
+            alignItems: 'center',
+            textAlign: 'center',
+          }}>
+          CASHLESS PAYMENT
+        </Text>
       </View>
 
-      <TouchableOpacity style={{width: 250, height: 250, borderRadius: 10, marginTop: 80, backgroundColor: '#FFC8AF', alignSelf: 'center'}} 
-      onPress={() => navigation.navigate('CashlessSuccess')}></TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          width: 250,
+          height: 250,
+          borderRadius: 10,
+          marginTop: 80,
+          backgroundColor: '#FFC8AF',
+          alignSelf: 'center',
+        }}
+        onPress={() =>
+          navigation.navigate('CashlessSuccess')
+        }></TouchableOpacity>
 
-      <Text style={{
+      <Text
+        style={{
           fontSize: 14,
           width: 247,
           height: 42,
-          alignSelf:'center',
-          textAlign:'center',
-          fontWeight:'bold',
+          alignSelf: 'center',
+          textAlign: 'center',
+          fontWeight: 'bold',
           marginTop: 30,
-          }}>
-              Silahkan melakukan pembayaran pada QR code di atas
+        }}>
+        Silahkan melakukan pembayaran pada QR code di atas
       </Text>
-      <TouchableOpacity style={{
+      <TouchableOpacity
+        style={{
           backgroundColor: '#FFC8AF',
           height: 41,
           width: 100,
           borderRadius: 10,
           marginTop: 70,
-          alignSelf: 'center'
-      }} onPress={() => navigation.navigate('UserMenu')}>
-          <Text style={{
-              fontSize: 14,
-              fontWeight: 'bold',
-              alignSelf: 'center',
-              marginTop: 10,
-          }}>KEMBALI</Text>
+          alignSelf: 'center',
+        }}
+        onPress={() => navigation.navigate('UserMenu')}>
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: 'bold',
+            alignSelf: 'center',
+            marginTop: 10,
+          }}>
+          KEMBALI
+        </Text>
       </TouchableOpacity>
     </View>
   );
--- component/StatusPesanan.js
+++ component/StatusPesanan.js
@@ -3,53 +3,94 @@ import {View, Text, FlatList, Image, TouchableOpacity} from 'react-native';
 
 function StatusPesanan() {
   return (
-    <View style= {{backgroundColor: '#FFF1E1'}}>
+    <View style={{backgroundColor: '#FFF1E1'}}>
       <View style={{backgroundColor: '#FFB84C'}}>
-        <Text style= {{
-          fontSize: 24, 
-          color: 'white', 
-          fontWeight: 'bold',
-          marginTop: 11,
-          marginBottom: 12,
-          alignItems: 'center',
-          textAlign: 'center',
+        <Text
+          style={{
+            fontSize: 24,
+            color: 'white',
+            fontWeight: 'bold',
+            marginTop: 11,
+            marginBottom: 12,
+            alignItems: 'center',
+            textAlign: 'center',
           }}>
-            Status Pesanan
-        </Text>  
+          Status Pesanan
+        </Text>
       </View>
       <FlatList
-          data={[...Array(3)]}
-          showsVerticalScrollIndicator={true}
-          keyExtractor={(a, i) => i + 'a'}
-          renderItem={({item, index}) => (
-            <TouchableOpacity
+        data={[...Array(3)]}
+        showsVerticalScrollIndicator={true}
+        keyExtractor={(a, i) => i + 'a'}
+        renderItem={({item, index}) => (
+          <TouchableOpacity
+            style={{
+              height: 120,
+              width: 350,
+              backgroundColor: '#FFC8AF',
+              elevation: 5,
+              marginVertical: 10,
+              flexDirection: 'row',
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              alignSelf: 'center',
+              borderRadius: 10,
+            }}>
+            <View
               style={{
-                height: 120,
-                width: 350,
-                backgroundColor: '#FFC8AF',
-                elevation: 5,
-                marginVertical: 10,
-                flexDirection: 'row',
-                paddingVertical: 10,
-                paddingHorizontal: 10,
-                alignSelf: 'center',
-                borderRadius: 10,
+                height: 100,
+                width: 100,
+                backgroundColor: '#C85252',
+                borderRadius: 20,
               }}>
-                <View style={{height:100, width:100, backgroundColor: '#C85252', borderRadius: 20}}>
-                  <Image style={{height:100, width:100}} source={require('../assets/icon/cooking.png')}/>
-                </View>
-                <View>
-                  <Text style={{marginLeft: 10, fontSize: 14, color:'black', fontWeight: 'bold'}}>Pesanan Anda Sedang Dimasak</Text>
-                  <Text style={{marginLeft: 10, fontSize: 14, color:'black', fontWeight: 'bold'}}>Tunggu Sebentar Lagi Yaa</Text> 
-                  <Text style={{marginLeft: 10, marginTop: 10,fontSize: 12, fontWeight: 'normal'}}>Order Number : AC291</Text>
-                  <Text style={{marginLeft: 10, marginTop: 10,fontSize: 12, fontWeight: 'normal'}}>Nomor Meja : 01</Text>
-                </View>
-            </TouchableOpacity>
-          )}
-        />
+              <Image
+                style={{height: 100, width: 100}}
+                source={require('../assets/icon/cooking.png')}
+              />
+            </View>
+            <View>
+              <Text
+                style={{
+                  marginLeft: 10,
+                  fontSize: 14,
+                  color: 'black',
+                  fontWeight: 'bold',
+                }}>
+                Pesanan Anda Sedang Dimasak
+              </Text>
+              <Text
+                style={{
+                  marginLeft: 10,
+                  fontSize: 14,
+                  color: 'black',
+                  fontWeight: 'bold',
+                }}>
+                Tunggu Sebentar Lagi Yaa
+              </Text>
+              <Text
+                style={{
+                  marginLeft: 10,
+                  marginTop: 10,
+                  fontSize: 12,
+                  fontWeight: 'normal',
+                }}>
+                Order Number : AC291
+              </Text>
+              <Text
+                style={{
+                  marginLeft: 10,
+                  marginTop: 10,
+                  fontSize: 12,
+                  fontWeight: 'normal',
+                }}>
+                Nomor Meja : 01
+              </Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
 
 export {StatusPesanan};
-
